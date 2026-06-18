22dff8f11f886955a20a4b4e1fd376fed2ae5a9f
fireee2316 <fireee@cgd.cx>
2026-06-18 23:50:27 +0000

    Update sendtogit.sh

--- sendtogit.sh
+++ sendtogit.sh
@@ -1,5 +1,20 @@
 sudo cp /etc/nixos/configuration.nix ~/jackconfig/myconfig.nix
+echo "config copy successful"
+
+sudo cp ~/.config/hypr/hyprland.conf ~/jackconfig/binds.nix
+echo "binds copy successful"
+
 cd ~/jackconfig
+echo "cd successful"
+
 git add myconfig.nix
+echo "git add successful"
+
+git add mybinds.conf
+echo "git add successful"
+
 git commit -m "${1:-config update $(date)}"
+echo "git commit successful"
+
 git push
+echo "push successful"
