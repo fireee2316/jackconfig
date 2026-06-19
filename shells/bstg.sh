94eaed91a697c260a3d195aa478def16e4df4f9d
fireee2316 <fireee@cgd.cx>
2026-06-19 00:21:44 +0000

    Update bstg.sh

--- shells/bstg.sh
+++ shells/bstg.sh
@@ -1,13 +1,13 @@
-sudo cp /etc/nixos/configuration.nix ~/jackconfig/configs/myconfig.nix || echo "config copy successful"
+sudo cp /etc/nixos/configuration.nix ~/jackconfig/configs/myconfig.nix > /dev/null || echo "config copy successful"
 
-sudo cp ~/.config/hypr/hyprland.conf ~/jackconfig/configs/mybinds.conf || echo "binds copy successful"
+sudo cp ~/.config/hypr/hyprland.conf ~/jackconfig/configs/mybinds.conf > /dev/null || echo "binds copy successful"
 
 cd ~/jackconfig
 
-git add configs/myconfig.nix || echo "git add config failed"
+git add configs/myconfig.nix > /dev/null || echo "git add config failed"
 
-git add configs/mybinds.conf || echo "git add binds failed"
+git add configs/mybinds.conf > /dev/null || echo "git add binds failed"
 
-git commit -m "${1:-config update $(date)}" && echo "git commit successful"
+git commit -m "${1:-config update $(date)}" > /dev/null && echo "git commit successful"
 
-git push && echo "push successful"
+git push > /dev/null && echo "push successful"
