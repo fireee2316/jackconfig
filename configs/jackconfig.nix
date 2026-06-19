30ba6e17006b18e83fe4f1f814eaff5d0a3eef67
jack <fireee@cgd.cx>
2026-06-19 15:22:44 +0100

    config update Fri Jun 19 03:22:44 PM BST 2026

--- configs/jackconfig.nix
+++ configs/jackconfig.nix
@@ -1,6 +1,11 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+
+ 
+# nigger
+
  
 { config, lib, pkgs, ... }:
 
