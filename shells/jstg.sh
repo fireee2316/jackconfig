b1d3c2c14b3bc07d3326b68c7f3e6ea8428453ff
fireee2316 <fireee@cgd.cx>
2026-06-19 00:16:31 +0000

    Update jstg.sh

--- shells/jstg.sh
+++ shells/jstg.sh
@@ -1,1 +1,13 @@
+sudo cp /etc/nixos/configuration.nix ~/jackconfig/configs/jackconfig.nix || echo "config copy failed"
 
+# sudo cp ~/.config/hypr/hyprland.conf ~/jackconfig/configs/mybinds.conf || echo "binds copy successful"
+
+cd ~/jackconfig
+
+git add configs/jackconfig.nix || echo "git add config failed"
+
+# git add configs/jackbinds.conf || echo "git add binds failed"
+
+git commit -m "${1:-config update $(date)}" && echo "git commit successful"
+
+git push && echo "push successful"
