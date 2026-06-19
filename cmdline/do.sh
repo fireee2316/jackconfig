f0e1e46e37b78b4fdaed2056a67fb367d57d577c
fireee2316 <fireee@cgd.cx>
2026-06-19 15:03:54 +0000

    Update do.sh

--- cmdline/do.sh
+++ cmdline/do.sh
@@ -1,2 +1,12 @@
 cd ~/jackconfig/cmdline
 ./cmdshell.sh > output.txt 
+
+git add output.txt > /dev/null || echo "git add output failed"
+
+# git add configs/jackbinds.conf > /dev/null || echo "git add binds failed"
+
+git commit -m "${1:-config update $(date)}" > /dev/null || echo "git commit failed"
+
+git push > /dev/null && echo "push successful"
+
+git restore ~/jackconfig/cmdline > /dev/null
